b2344cbb9e3ef9a7ba1c2fe99a3e66ef6c477b87
Katelyn Gigante <clockwork.singularity@gmail.com>
2016-12-14 16:48:36 +1100

    Don't try to spawn if already spawning.

--- spawner.js
+++ spawner.js
@@ -9,15 +9,19 @@ var controller = {
         }
     }
 
+    if (Game.spawns['Spawn1'].spawning){
+        return;
+    }
+
     roles = [
         {role: 'harvester', n: 2}, 
-        {role: 'builder', n:1},
+        {role: 'builder', n:3},
         {role: 'upgrader', n:1}
         ];
     for (var r in roles) {
         role = roles[r];
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == role.role);
-        console.log(role.role + ": " + harvesters + "/" + role.n);
+        //console.log(role.role + ": " + harvesters + "/" + role.n);
 
         if(harvesters.length < role.n) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: role.role});
