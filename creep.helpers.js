2d5750b32ff0b40ec897da77149bacc74ed34c3a
Katelyn Gigante <clockwork.singularity@gmail.com>
2016-12-15 13:28:37 +1100

    Use containers

--- creep.helpers.js
+++ creep.helpers.js
@@ -1,10 +1,27 @@
 var creepHelpers = {
 
     /** @param {Creep} creep **/
+    // moveToSource: function(creep) {
+    //         var source = creep.pos.findClosestByPath(FIND_SOURCES);
+    //         if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+    //             creep.moveTo(source, {reusePath: 20});
+    //         }
+    // },
     moveToSource: function(creep) {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {reusePath: 20});
+            let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: s => (s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_STORAGE) &&
+                             s.store[RESOURCE_ENERGY] > 0
+            });
+            if (container != undefined) {
+                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container, {reusePath: 20});
+                }
+            }
+            else {
+                var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source, {reusePath: 20});
+                }
             }
     }
 }
