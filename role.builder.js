122b89c27c5b4cf5efe7783e319d966f11eafd86
Katelyn Gigante <clockwork.singularity@gmail.com>
2016-12-19 01:22:55 +1100

    builders should do .working too

--- role.builder.js
+++ role.builder.js
@@ -7,16 +7,16 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
+        if(creep.memory.working && creep.carry.energy == 0) {
+            creep.memory.working = false;
             creep.say('collecting');
         }
-        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.building = true;
+        if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.working = true;
             creep.say('building');
         }
 
-        if(creep.memory.building) {
+        if(creep.memory.working) {
             var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
             if(target) {
                 if(creep.build(target) == ERR_NOT_IN_RANGE) {
